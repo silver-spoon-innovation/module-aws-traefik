dbf52b1c9630acb4b29f28cd73822ba4e79a20da
pamornpolh <pamornpol.hanmatheekuna@gmail.com>
2022-12-29 22:53:29 +0700

    add aws provider profile

--- main.tf
+++ main.tf
@@ -1,16 +1,17 @@
 provider "aws" {
   region = var.aws_region
-}
-
-data "aws_eks_cluster_auth" "ms-sssm" {
-  name = var.kubernetes_cluster_id
+  profile = "default"
 }
 
 provider "helm" {
   kubernetes {
     cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
     host                   = var.kubernetes_cluster_endpoint
-    token                  = data.aws_eks_cluster_auth.ms-sssm.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
+      command     = "aws"
+    }
   }
 }
 
